50af458da5c9da1cb0de6ebcc97f3266685ddef4
Jose Manuel Enriquez Baena <joseenriquezbaena@gmail.com>
2026-05-08 16:35:50 +0200

    feat: Inicializacion ble

---
+++ components/HapticController/HapticController.cpp
@@ -0,0 +1,309 @@
+#include "include/HapticController.hpp"
+
+/** 
+ *  =======================================
+ *  DECLARACION DE MACROS 
+ *  =======================================
+ */
+#define BLE_TAG "BLE"
+#define GATTS_TAG "GATTS"
+#define APP_PROFILE_ID 0
+#define adv_config_flag      (1 << 0)
+#define scan_rsp_config_flag (1 << 1)
+
+/** 
+ *  =======================================
+ *  DECLARACIONES STATIC
+ *  =======================================
+ */
+
+static void gatts_profile_event_handler(esp_gatts_cb_event_t event, 
+                                        esp_gatt_if_t gatts_if, 
+                                        esp_ble_gatts_cb_param_t *param);
+
+static void gatts_event_handler(esp_gatts_cb_event_t event, 
+                                esp_gatt_if_t gatts_if, 
+                                esp_ble_gatts_cb_param_t *param);
+
+static void gap_event_handler(esp_gap_ble_cb_event_t event, 
+                                esp_ble_gap_cb_param_t *param);
+
+
+/* =========== VARIABLES DE PROFILE =========== */
+
+/* Struct para configurar el profile */
+struct gatts_profile_inst {
+    esp_gatts_cb_t gatts_cb;
+    uint16_t gatts_if;
+    uint16_t app_id;
+    uint16_t conn_id;
+    uint16_t service_handle;
+    esp_gatt_srvc_id_t service_id;
+    uint16_t char_handle;
+    esp_bt_uuid_t char_uuid;
+    esp_gatt_perm_t perm;
+    esp_gatt_char_prop_t property;
+    uint16_t descr_handle;
+    esp_bt_uuid_t descr_uuid;
+};
+
+/* Unico profile que va a tener el esp32*/
+static struct gatts_profile_inst app_profile = {
+    .gatts_cb = gatts_profile_event_handler,
+    .gatts_if = ESP_GATT_IF_NONE,
+};
+
+/* =========================================== */
+/*         VARIABLES DE AVISOS Y ESCANEO       */
+
+/* Variable que controla si se ha configurado los avisos. Es un array de flags */
+static uint8_t adv_config_done = 0;
+
+static uint8_t adv_service_uuid128[32] = {
+    /* LSB <--------------------------------------------------------------------------------> MSB */
+    //first uuid, 16bit, [12],[13] is the value
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xEE, 0x00, 0x00, 0x00,
+    //second uuid, 32bit, [12], [13], [14], [15] is the value
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00,
+};
+
+/* struct con los parametros de avisos */
+static esp_ble_adv_params_t adv_params = {
+    .adv_int_min        = ESP_BLE_GAP_ADV_ITVL_MS(20),
+    .adv_int_max        = ESP_BLE_GAP_ADV_ITVL_MS(40),
+    .adv_type           = ADV_TYPE_IND,
+    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
+    .channel_map        = ADV_CHNL_ALL,
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+};
+
+/* struct con los datos que contiene el aviso */
+static esp_ble_adv_data_t adv_data = {
+    .set_scan_rsp = false,
+    .include_name = true,
+    .include_txpower = false,
+    .min_interval = ESP_BLE_GAP_CONN_ITVL_MS(7.5), //slave connection min interval
+    .max_interval = ESP_BLE_GAP_CONN_ITVL_MS(20), //slave connection max interval
+    .appearance = 0x00,
+    .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
+    .p_manufacturer_data =  NULL, //&test_manufacturer[0],
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = sizeof(adv_service_uuid128),
+    .p_service_uuid = adv_service_uuid128,
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
+};
+
+/* struct con los datos que contiene el escaneo */
+static esp_ble_adv_data_t scan_rsp_data = {
+    .set_scan_rsp = true,
+    .include_name = true,
+    .include_txpower = true,
+    //.min_interval = 0x0006,
+    //.max_interval = 0x0010,
+    .appearance = 0x00,
+    .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
+    .p_manufacturer_data =  NULL, //&test_manufacturer[0],
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = sizeof(adv_service_uuid128),
+    .p_service_uuid = adv_service_uuid128,
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
+};
+
+/* =========================================== */
+
+
+/* ================================================ */
+/*    IMPLEMENTACION DE LA CLASE HapticController   */
+
+HapticController::HapticController(int _pin_gpio): pin_gpio(_pin_gpio){}
+
+void HapticController::init(){
+    esp_err_t ret;
+
+    // 1. Inicializar NVS.
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    // 2. Inicializar Host Controller
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
+    
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    ret = esp_bt_controller_init(&bt_cfg);
+    if (ret) {
+        ESP_LOGE(GATTS_TAG, "%s initialize controller failed: %s", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    ret = esp_bt_controller_enable(ESP_BT_MODE_BLE);
+    if (ret) {
+        ESP_LOGE(GATTS_TAG, "%s enable controller failed: %s", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    // 3. Inicializar software que implementa protocolos como L2CAP, ATT...
+    esp_bluedroid_config_t cfg = BT_BLUEDROID_INIT_CONFIG_DEFAULT();
+    ret = esp_bluedroid_init_with_cfg(&cfg);
+    if (ret) {
+        ESP_LOGE(GATTS_TAG, "%s init bluetooth failed: %s", __func__, esp_err_to_name(ret));
+        return;
+    }
+    ret = esp_bluedroid_enable();
+    if (ret) {
+        ESP_LOGE(GATTS_TAG, "%s enable bluetooth failed: %s", __func__, esp_err_to_name(ret));
+        return;
+    }
+
+    // 4. Registrar ciclo de vida del GATT
+    ret = esp_ble_gatts_register_callback(gatts_event_handler);
+    if (ret){
+        ESP_LOGE(GATTS_TAG, "gatts register error, error code = %x", ret);
+        return;
+    }
+
+    // 5. Registrar ciclo de vida del GAP
+    ret = esp_ble_gap_register_callback(gap_event_handler);
+    if (ret){
+        ESP_LOGE(GATTS_TAG, "gap register error, error code = %x", ret);
+        return;
+    }
+
+    // 6. Registrar profile
+    ret = esp_ble_gatts_app_register(APP_PROFILE_ID);
+    if (ret){
+        ESP_LOGE(GATTS_TAG, "gatts app register error, error code = %x", ret);
+        return;
+    }
+
+    // Configuramos el tamaño maximo de los paquetes ble
+    esp_err_t local_mtu_ret = esp_ble_gatt_set_local_mtu(500);
+    if (local_mtu_ret){
+        ESP_LOGE(GATTS_TAG, "set local  MTU failed, error code = %x", local_mtu_ret);
+    }
+}
+
+/* ================================================ */
+
+/*  ================================================ */
+/*         IMPLEMENTACION FUNCIONES BLUETOOTH        */
+ 
+/* Esta funcion se encarga de manejar los eventos del app_profile */
+static void gatts_profile_event_handler(esp_gatts_cb_event_t event, 
+                                        esp_gatt_if_t gatts_if, 
+                                        esp_ble_gatts_cb_param_t *param){
+    switch (event) {
+    case ESP_GATTS_REG_EVT: // Se configura los datos de escaneo y avisos
+        ESP_LOGI(GATTS_TAG, "GATT server register, status %d, app_id %d, gatts_if %d", param->reg.status, param->reg.app_id, gatts_if);
+        app_profile.service_id.is_primary = true;
+        app_profile.service_id.id.inst_id = 0x00;
+        app_profile.service_id.id.uuid.len = ESP_UUID_LEN_16;
+        app_profile.service_id.id.uuid.uuid.uuid16 = GATTS_SERVICE_UUID_TEST_A; // CAMBIAR
+
+        esp_err_t set_dev_name_ret = esp_ble_gap_set_device_name("pulsera-vibracion");
+        if (set_dev_name_ret){
+            ESP_LOGE(GATTS_TAG, "set device name failed, error code = %x", set_dev_name_ret);
+        }
+
+        //config adv data
+        esp_err_t ret = esp_ble_gap_config_adv_data(&adv_data);
+        if (ret){
+            ESP_LOGE(GATTS_TAG, "config adv data failed, error code = %x", ret);
+        }
+        adv_config_done |= adv_config_flag; // Indicamos que se ha configurado en el array de flags
+
+        //config scan response data
+        ret = esp_ble_gap_config_adv_data(&scan_rsp_data);
+        if (ret){
+            ESP_LOGE(GATTS_TAG, "config scan response data failed, error code = %x", ret);
+        }
+        adv_config_done |= scan_rsp_config_flag; // Indicamos que se ha configurado en el array de flags
+
+        esp_ble_gatts_create_service(gatts_if, &app_profile.service_id, 0); // CAMBIAR EL TERCERO
+        break;
+    }
+}
+
+/* Esta funcion se encarga de registrar el numero de interfaz al app_profile y 
+de llamar a la callback correspondiente */
+static void gatts_event_handler(esp_gatts_cb_event_t event, 
+                                esp_gatt_if_t gatts_if, 
+                                esp_ble_gatts_cb_param_t *param)
+{
+    /* Cuando ya hemos registrado la funcion y genera el evento, lo usamos para inicialziar
+    la interfaz del app_profile */
+    if (event == ESP_GATTS_REG_EVT) {
+        if (param->reg.status == ESP_GATT_OK) {
+            app_profile.gatts_if = gatts_if;
+        } else {
+            ESP_LOGI(GATTS_TAG, "Reg app failed, app_id %04x, status %d",
+                    param->reg.app_id,
+                    param->reg.status);
+            return;
+        }
+    }
+
+    /* Comprobamos que el evento capturado tiene el mismo numero de interfaz que el 
+    app_profile y llamamos a la callback correspondiente de ese perfil */
+    if( (gatts_if == ESP_GATT_IF_NONE) || (gatts_if == app_profile.gatts_if) ){
+        if(app_profile.gatts_cb){
+            app_profile.gatts_cb(event, gatts_if, param);
+        }
+    }
+}
+
+/* Esta funcion se encarga de gestionar los eventos para configurar el esp32 como peripheral */
+static void gap_event_handler(esp_gap_ble_cb_event_t event, 
+                            esp_ble_gap_cb_param_t *param)
+{
+    switch (event) {
+    case ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT: // Salta cuando se ha configurado los datos de avisos
+        adv_config_done &= (~adv_config_flag); // Limpiamo flag
+        if (adv_config_done == 0){
+            esp_ble_gap_start_advertising(&adv_params);
+        }
+        break;
+    case ESP_GAP_BLE_SCAN_RSP_DATA_SET_COMPLETE_EVT: // Salta cuando se ha configurado los datos de escaneo
+        adv_config_done &= (~scan_rsp_config_flag); // Limpiamo flag
+        if (adv_config_done == 0){
+            esp_ble_gap_start_advertising(&adv_params);
+        }
+        break;
+    case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:
+        //advertising start complete event to indicate advertising start successfully or failed
+        if (param->adv_start_cmpl.status != ESP_BT_STATUS_SUCCESS) {
+            ESP_LOGE(GATTS_TAG, "Advertising start failed, status %d", param->adv_start_cmpl.status);
+            break;
+        }
+        ESP_LOGI(GATTS_TAG, "Advertising start successfully");
+        break;
+    case ESP_GAP_BLE_ADV_STOP_COMPLETE_EVT:
+        if (param->adv_start_cmpl.status != ESP_BT_STATUS_SUCCESS) {
+            ESP_LOGE(GATTS_TAG, "Advertising stop failed, status %d", param->adv_stop_cmpl.status);
+            break;
+        }
+        ESP_LOGI(GATTS_TAG, "Advertising stop successfully");
+        break;
+    case ESP_GAP_BLE_UPDATE_CONN_PARAMS_EVT:
+         ESP_LOGI(GATTS_TAG, "Connection params update, status %d, conn_int %d, latency %d, timeout %d",
+                  param->update_conn_params.status,
+                  param->update_conn_params.conn_int,
+                  param->update_conn_params.latency,
+                  param->update_conn_params.timeout);
+        break;
+    case ESP_GAP_BLE_SET_PKT_LENGTH_COMPLETE_EVT:
+        ESP_LOGI(GATTS_TAG, "Packet length update, status %d, rx %d, tx %d",
+                  param->pkt_data_length_cmpl.status,
+                  param->pkt_data_length_cmpl.params.rx_len,
+                  param->pkt_data_length_cmpl.params.tx_len);
+        break;
+    default:
+        break;
+    }
+}
+
+/* ================================================ */
---
+++ components/HapticController/include/HapticController.hpp
@@ -0,0 +1,33 @@
+#ifndef HAPTICCONTROLLER_HPP
+#define HAPTICCONTROLLER_HPP
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_bt.h"
+
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#include "esp_gatt_common_api.h"
+
+#include "sdkconfig.h"
+
+class HapticController{
+    private:
+        int pin_gpio;
+    public:
+        HapticController(int _pin_gpio);
+        void init();
+};
+
+#endif
